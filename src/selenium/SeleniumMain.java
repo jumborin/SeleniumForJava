560c0dbb4647379618fc59d671a1aa70180bd729
jumborin <y.f.architect@gmail.com>
2019-01-20 21:57:32 +0900

    クラスを分けた

---
+++ src/selenium/SeleniumMain.java
@@ -0,0 +1,18 @@
+package selenium;
+
+/**
+ * Seleniumテスト用クラス
+ * @author jumborin
+ *
+ */
+public class SeleniumMain {
+
+	/**
+	 * 実行メインクラス
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SeleniumSample.openGoogle();
+		SeleniumSample.openGoogleMap();
+	}
+}
---
+++ src/selenium/SeleniumSample.java
@@ -0,0 +1,60 @@
+package selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Seleniumテスト用クラス
+ * @author jumborin
+ *
+ */
+public class SeleniumSample {
+
+	/** GoogleChromeのドライバークラス名 */
+	private final static String CHROME_DRIVER_CLASS = "webdriver.chrome.driver";
+
+	/** GoogleChromeのドライバーファイル */
+	private final static String CHROME_DRIVER_PATH = "driver/chromedriver.exe";
+
+	/**
+	 * Googleを開く。
+	 */
+	public static void openGoogle() {
+		//登録
+		System.setProperty(CHROME_DRIVER_CLASS, CHROME_DRIVER_PATH);
+
+		//インスタンス作成
+		WebDriver driver = new ChromeDriver();
+
+		//グーグルに遷移
+		driver.get("https://www.google.co.jp/");
+
+		// タイトルを出力する。
+		System.out.println(driver.getTitle());
+
+		//URLを出力する
+		System.out.println(driver.getCurrentUrl());
+
+	}
+
+	/**
+	 * GoogleMapを開いて検索をする。
+	 */
+	public static void openGoogleMap() {
+		//登録
+		System.setProperty(CHROME_DRIVER_CLASS, CHROME_DRIVER_PATH);
+
+		//インスタンス作成
+		WebDriver driver = new ChromeDriver();
+
+		//グーグルマップに遷移する
+		driver.get("https://www.google.co.jp/maps/");
+
+		//検索窓に入力する
+		driver.findElement(By.id("searchboxinput")).sendKeys("test");
+
+		//検索ボタンをクリックする。
+		driver.findElement(By.id("searchbox-searchbutton")).click();
+	}
+}
