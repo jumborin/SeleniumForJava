fbe2d4683f22e15d049af60cdae6c15590a2f104
jumborin <y.f.architect@gmail.com>
2019-01-20 15:32:02 +0900

    ソースコードのフォーマットをスペースからタブに変更しクラスPathを変更

--- src/selenium/SeleniumCode.java
+++ src/selenium/SeleniumCode.java
@@ -5,28 +5,33 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumCode {
+	private final static String CHROME_DRIVER_CLASS = "webdriver.chrome.driver";
+	private final static String CHROME_DRIVER_PATH = "libs/chromedriver.exe";
 
-	    public static void main(String[] args) {
-	        System.setProperty("webdriver.chrome.driver", "libs/chromedriver.exe");
-	        WebDriver driver = new ChromeDriver();
-	        driver.get("https://www.google.co.jp/");
+	public static void main(String[] args) {
+		//登録
+		System.setProperty(CHROME_DRIVER_CLASS, CHROME_DRIVER_PATH);
 
-	        // タイトルを出力する。
-	        System.out.println(driver.getTitle());
+		//インスタンス作成
+		WebDriver driver = new ChromeDriver();
 
-	        //URLを出力する
-	        System.out.println(driver.getCurrentUrl());
+		//グーグルに遷移
+		driver.get("https://www.google.co.jp/");
 
-	        //グーグルマップに遷移する
-	        driver.get("https://www.google.co.jp/maps/");
+		// タイトルを出力する。
+		System.out.println(driver.getTitle());
 
-	        //検索窓に入力する
-	        driver.findElement(By.id("searchboxinput")).sendKeys("test");
+		//URLを出力する
+		System.out.println(driver.getCurrentUrl());
 
-	        //検索ボタンをクリックする。
-	        driver.findElement(By.id("searchbox-searchbutton")).click();
+		//グーグルマップに遷移する
+		driver.get("https://www.google.co.jp/maps/");
 
+		//検索窓に入力する
+		driver.findElement(By.id("searchboxinput")).sendKeys("test");
 
-	    }
+		//検索ボタンをクリックする。
+		driver.findElement(By.id("searchbox-searchbutton")).click();
 
 	}
+}
