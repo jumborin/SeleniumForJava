4cb7074cff33682e31a050f7058f3088ad39f9f5
jumborin <y.f.architect@gmail.com>
2019-01-24 07:38:09 +0900

    Junit5対応

--- src/test/InternetExplorerSeleniumTestPage.java
+++ src/test/InternetExplorerSeleniumTestPage.java
@@ -2,11 +2,12 @@ package test;
 
 import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
 import base.InternetExplorerSeleniumTestPageBase;
@@ -21,28 +22,28 @@ public class InternetExplorerSeleniumTestPage extends InternetExplorerSeleniumTe
 	/**
 	 * @throws java.lang.Exception
 	 */
-	@BeforeClass
+	@BeforeAll
 	public static void setUpBeforeClass() throws Exception {
 	}
 
 	/**
 	 * @throws java.lang.Exception
 	 */
-	@AfterClass
+	@AfterEach
 	public static void tearDownAfterClass() throws Exception {
 	}
 
 	/**
 	 * @throws java.lang.Exception
 	 */
-	@Before
+	@BeforeEach
 	public void setUp() throws Exception{
 		super.setUp("http://localhost/");
 	}
 
 	/**
 	 */
-	@After
+	@AfterAll
 	public void tearDown() {
 	}
 
@@ -52,42 +53,49 @@ public class InternetExplorerSeleniumTestPage extends InternetExplorerSeleniumTe
 	 */
 	@Override
 	@Test
+	@DisplayName("タイトルが想定通りになっているかのテスト")
 	public void testTitleTest() {
 		assertEquals("localhost", super.driver.getTitle());
 	}
 
 	@Override
 	@Test
+	@DisplayName("URLが想定通りになっているかのテスト")
 	public void testUrlTest() {
 		assertEquals("http://localhost/", super.driver.getCurrentUrl());
 	}
 
 	@Override
 	@Test
+	@DisplayName("ヘッダーが想定通りになっているかのテスト")
 	public void testFirstDisplayHeaderTest() {
 		assertEquals("a", super.driver.findElement(By.id("t")).getText());
 	}
 
 	@Override
 	@Test
+	@DisplayName("フッターが想定通りになっているかのテスト")
 	public void testFirstDisplayFooterTest() {
 		assertEquals("", super.driver.findElement(By.id("t")).getText());
 	}
 
 	@Override
 	@Test
+	@DisplayName("コンテンツ部分の初期表示が想定通りになっているかのテスト")
 	public void testFirstDisplayContentsTest() {
 		assertEquals("", super.driver.findElement(By.id("t")).getText());
 	}
 
 	@Override
 	@Test
+	@DisplayName("入力チェックが想定通りになっているかのテスト")
 	public void testInputChectTest() {
 		assertEquals("", super.driver.findElement(By.id("t")).getText());
 	}
 
 	@Override
 	@Test
+	@DisplayName("正常登録時の処理が想定通りになっているかのテスト")
 	public void testNormalSubmitTest() {
 		assertEquals("", super.driver.findElement(By.id("t")).getText());
 	}
